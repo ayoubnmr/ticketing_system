709caa0f256bc96d3ca35e1c15638a17933f3dd4
ayoub <anmr06765@gmail.com>
2022-06-26 15:58:55 +0300

    done the job

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -10,10 +10,10 @@ class Ticket < ApplicationRecord
   after_save :perform
 
   def perform
-    if Date.today == self.end && (status = 1)
-      project.users.each do |user|
+    # if Date.today == self.end && (status = 1) 
+    project.users.each do |user|
         TicketEndJob.set(wait_until: self.end.to_time).perform_later(self, project ,user )
-      end
+      
     end
   end 
 end
